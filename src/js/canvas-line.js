01eea91289ecfa981e1d070913b06a2f2cc1f206
chengquan223 <309581454@qq.com>
2017-04-26 18:22:15 +0800

    update canvas-line animate

--- src/js/canvas-line.js
+++ src/js/canvas-line.js
@@ -9,21 +9,19 @@ var TrackLine = function (userOptions) {
     var self = this,
         canvas = document.getElementById('myCanvas'),
         context = canvas.getContext('2d'),
-        width = canvas.width = window.innerWidth,
-        height = canvas.height = window.innerHeight,
+        width = canvas.width || window.innerWidth,
+        height = canvas.height || window.innerHeight,
         lines = []; //所有线条
+    canvas.style.cssText = "position:absolute;" + "left:0;" + "top:0;" + "z-index:0;user-select:none;";
 
     //默认参数
     var options = {
         lineWidth: 1,
-        data: [{
-            points: [
-                [50, 50],
-                [150, 250],
-                [400, 650]
-            ],
-            color: 'blue'
-        }]
+        strokeStyle: '#fff',
+        isShowLine: true,
+        isAnimate: true,
+        radius: 2,
+        speed: 1
     };
 
     //参数合并
@@ -36,18 +34,18 @@ var TrackLine = function (userOptions) {
     //线条
     function Line(options) {
         this.options = options;
-        this.color = options.color;
-        this.turnPoints = options.points; //[[50,50],[150,250],[400,650]]
+        this.fillStyle = options.fillStyle;
+        this.turnPoints = options.turnPoints || []; //[[50,50],[150,250],[400,650]]
         this.step = 0;
-        this.temp = 0;
-        this.getPointList();
+        this.pointList = options.pointList || this.getPointList();
     }
 
     Line.prototype.getPointList = function () {
-        var self = this;
-        var turnPoints = self.turnPoints;
-        self.pointList = [];
-        if (turnPoints.length < 2) return [];
+        var turnPoints = this.turnPoints;
+        var pointList = this.pointList = [];
+        if (turnPoints.length < 2) {
+            return [];
+        }
         for (var i = 0; i < turnPoints.length; i++) {
             var start = turnPoints[i];
             var end = turnPoints[i + 1];
@@ -56,22 +54,27 @@ var TrackLine = function (userOptions) {
                 var vx = ((end[0] - start[0]) / distance);
                 var vy = ((end[1] - start[1]) / distance);
                 for (var j = 0; j < distance; j++) {
-                    self.pointList.push([start[0] + vx * j, start[1] + vy * j]);
+                    pointList.push([start[0] + vx * j, start[1] + vy * j]);
                 }
             }
         }
     }
 
     Line.prototype.draw = function (context) {
-        var self = this;
-        if (this.turnPoints.length >= 2) {
+        var pointList = this.turnPoints;
+        if (pointList.length >= 2) {
             context.save();
             context.beginPath();
             context.lineWidth = options.lineWidth;
-            context.strokeStyle = options.fillColor;
-            context.moveTo(this.turnPoints[0][0], this.turnPoints[0][1]);
-            for (var i = 0; i < this.turnPoints.length; i++) {
-                context.lineTo(this.turnPoints[i][0], this.turnPoints[i][1]);
+            context.strokeStyle = options.strokeStyle;
+            for (var i = 0; i < pointList.length; i++) {
+                var x = pointList[i][0];
+                var y = pointList[i][1];
+                if (i == 0) {
+                    context.moveTo(x, y);
+                } else {
+                    context.lineTo(x, y);
+                }
             }
             context.stroke();
             context.restore();
@@ -82,26 +85,17 @@ var TrackLine = function (userOptions) {
         var pointList = this.pointList || this.getPointList();
 
         context.save();
-        context.fillStyle = this.color;
+        context.fillStyle = this.fillStyle;
         // context.shadowColor = options.shadowColor;
         // context.shadowBlur = options.shadowBlur;
         context.beginPath();
-        context.arc(pointList[this.step][0], pointList[this.step][1], 3, 0, Math.PI * 2, true);
-        if (this.temp > 0) {
-            context.arc(pointList[this.temp][0], pointList[this.temp][1], 3, 0, Math.PI * 2, true);
-        }
+        context.arc(pointList[this.step][0], pointList[this.step][1], options.radius, 0, Math.PI * 2, true);
         context.fill();
         context.closePath();
         context.restore();
-        this.step += 1;
-        if (this.step > 150 || this.temp > 0) {
-            this.temp += 1;
-        }
+        this.step += options.speed;
         if (this.step >= pointList.length) {
             this.step = 0;
-        }
-        if (this.temp >= pointList.length) {
-            this.temp = 0;
         }
     }
 
@@ -117,17 +111,18 @@ var TrackLine = function (userOptions) {
 
     //渲染
     var render = function () {
-        context.fillStyle = 'rgba(0,0,0,.95)';
+        context.fillStyle = 'rgba(0,0,0,.88)';
         var prev = context.globalCompositeOperation;
         context.globalCompositeOperation = 'destination-in';
         context.fillRect(0, 0, width, height);
         context.globalCompositeOperation = prev;
 
         var lines = self.lines;
-        for (var i = 0; i < lines.length; i++) {
-            lines[i].draw(context);
-        }
-
+        // //静态背景线条渲染
+        // for (var i = 0; i < lines.length; i++) {
+        //     lines[i].draw(context);
+        // }
+        //动画渲染
         for (var j = 0; j < lines.length; j++) {
             lines[j].drawMoveCircle(context); //移动圆点
         }
