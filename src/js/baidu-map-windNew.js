d82420e624605ff2c944ab922cacfba44cef4868
chengquan223 <309581454@qq.com>
2017-06-16 13:28:30 +0800

    update baidu-map-windNew.js

--- src/js/baidu-map-windNew.js
+++ src/js/baidu-map-windNew.js
@@ -8,6 +8,7 @@ import {
 var Windy = function (map, userOptions) {
     var self = this;
 
+    //默认参数
     var options = {
         MAX_PARTICLE_AGE: 100,
         FRAME_RATE: 20,
@@ -16,7 +17,20 @@ var Windy = function (map, userOptions) {
         color: 'rgba(71,160,233,0.8)'
     };
 
+    //全局变量
+    var windLayer = null,
+        width = map.getSize().width,
+        height = map.getSize().height;
+
     //参数合并
+}
+
+Windy.prototype.start = function () {
+
+}
+
+Windy.prototype.stop = function () {
+
 }
 
 Windy.prototype.update = function (resetOpts) {
