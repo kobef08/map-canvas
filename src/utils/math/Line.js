d8c0188738a099d7d4107fff76c4375d11194cca
chengquan223 <309581454@qq.com>
2017-07-06 17:12:32 +0800

    新增直线常用计算公式

--- src/utils/math/Line.js
+++ src/utils/math/Line.js
@@ -1,7 +1,7 @@
 /**
  * 获取两点间距离
- * @param {any} p1 起点
- * @param {any} p2 终点
+ * @param {any} point1 起点
+ * @param {any} point2 终点
  */
 function getDistanceBy(p1, p2) {
     return Math.sqrt(Math.pow(p1[0] - p2[0]) + Math.pow(p1[1] - p2[1]));
