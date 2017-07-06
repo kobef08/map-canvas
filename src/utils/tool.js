51143fca8f89c2926eb9105a6e1a05fcd592d94e
chengquan223 <309581454@qq.com>
2017-07-06 17:02:08 +0800

    update tool.js

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -7,8 +7,7 @@ export default {
     //计算两点间距离
     getDistance: function (p1, p2) {
         return Math.sqrt(
-            (p1[0] - p2[0]) * (p1[0] - p2[0]) +
-            (p1[1] - p2[1]) * (p1[1] - p2[1])
+            (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1])
         );
     }
 }
