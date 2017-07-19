0fb54f9cd534a0b1c76d12cf9f7c4eef2ebed675
chengquan223 <309581454@qq.com>
2017-07-19 18:59:23 +0800

    update roundRect

--- src/canvas/helper/roundRect.js
+++ src/canvas/helper/roundRect.js
@@ -22,7 +22,16 @@ export default {
         if (typeof r === 'number') {
             r1 = r2 = r3 = r4 = r;
         } else if (r instanceof Array) {
-            r1 = r2 = r3 = r4 = r[0];
+            if (r.length === 1) {
+                r1 = r2 = r3 = r4 = r[0];
+            } else if (r.length === 2) {
+                r1 = r3 = r[0];
+                r2 = r4 = r[1];
+            } else if (r.length === 3) {
+                r1 = r[0];
+                r2 = r4 = r[1];
+                r3 = r[2];
+            }
         }
     }
 }
