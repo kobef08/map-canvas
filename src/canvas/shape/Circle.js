63709cf2ffe718ffd5414fb6fa0ee2d7cbdb8f50
chengquan223 <309581454@qq.com>
2017-07-19 13:26:24 +0800

    add shape Circle

---
+++ src/canvas/shape/Circle.js
@@ -0,0 +1,14 @@
+function Circle() {
+    this.type = 'circle';
+    this.shape = {
+        cx: 0,
+        cy: 0,
+        r: 0
+    };
+}
+
+Circle.prototype.draw = function (ctx, shape) {
+    ctx.arc(shape.cx, shape.cy, shape.r, 0, Math.PI * 2, true);
+}
+
+export default Circle;
