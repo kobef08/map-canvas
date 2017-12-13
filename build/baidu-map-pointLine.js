6b52e1fbf3e799fff598553304832d22f0068e63
chengquan223 <309581454@qq.com>
2017-12-13 11:50:20 +0800

    add baidu-map-pointline.html

---
+++ build/baidu-map-pointLine.js
@@ -0,0 +1,215 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.PointLine = factory());
+}(this, (function () { 'use strict';
+
+/**
+ * @author https://github.com/chengquan223
+ * @Date 2017-02-27
+ * */
+function CanvasLayer(options) {
+    this.options = options || {};
+    this.paneName = this.options.paneName || 'labelPane';
+    this.zIndex = this.options.zIndex || 0;
+    this._map = options.map;
+    this._lastDrawTime = null;
+    this.show();
+}
+
+CanvasLayer.prototype = new BMap.Overlay();
+
+CanvasLayer.prototype.initialize = function (map) {
+    this._map = map;
+    var canvas = this.canvas = document.createElement('canvas');
+    var ctx = this.ctx = this.canvas.getContext('2d');
+    canvas.style.cssText = 'position:absolute;' + 'left:0;' + 'top:0;' + 'z-index:' + this.zIndex + ';';
+    this.adjustSize();
+    this.adjustRatio(ctx);
+    map.getPanes()[this.paneName].appendChild(canvas);
+    var that = this;
+    map.addEventListener('resize', function () {
+        that.adjustSize();
+        that._draw();
+    });
+    return this.canvas;
+};
+
+CanvasLayer.prototype.adjustSize = function () {
+    var size = this._map.getSize();
+    var canvas = this.canvas;
+    canvas.width = size.width;
+    canvas.height = size.height;
+    canvas.style.width = canvas.width + 'px';
+    canvas.style.height = canvas.height + 'px';
+};
+
+CanvasLayer.prototype.adjustRatio = function (ctx) {
+    var backingStore = ctx.backingStorePixelRatio || ctx.webkitBackingStorePixelRatio || ctx.mozBackingStorePixelRatio || ctx.msBackingStorePixelRatio || ctx.oBackingStorePixelRatio || ctx.backingStorePixelRatio || 1;
+    var pixelRatio = (window.devicePixelRatio || 1) / backingStore;
+    var canvasWidth = ctx.canvas.width;
+    var canvasHeight = ctx.canvas.height;
+    ctx.canvas.width = canvasWidth * pixelRatio;
+    ctx.canvas.height = canvasHeight * pixelRatio;
+    ctx.canvas.style.width = canvasWidth + 'px';
+    ctx.canvas.style.height = canvasHeight + 'px';
+    // console.log(ctx.canvas.height, canvasHeight);
+    ctx.scale(pixelRatio, pixelRatio);
+};
+
+CanvasLayer.prototype.draw = function () {
+    var self = this;
+    var args = arguments;
+
+    clearTimeout(self.timeoutID);
+    self.timeoutID = setTimeout(function () {
+        self._draw();
+    }, 15);
+};
+
+CanvasLayer.prototype._draw = function () {
+    var map = this._map;
+    var size = map.getSize();
+    var center = map.getCenter();
+    if (center) {
+        var pixel = map.pointToOverlayPixel(center);
+        this.canvas.style.left = pixel.x - size.width / 2 + 'px';
+        this.canvas.style.top = pixel.y - size.height / 2 + 'px';
+        this.dispatchEvent('draw');
+        this.options.update && this.options.update.call(this);
+    }
+};
+
+CanvasLayer.prototype.getContainer = function () {
+    return this.canvas;
+};
+
+CanvasLayer.prototype.show = function () {
+    if (!this.canvas) {
+        this._map.addOverlay(this);
+    }
+    this.canvas.style.display = 'block';
+};
+
+CanvasLayer.prototype.hide = function () {
+    this.canvas.style.display = 'none';
+    //this._map.removeOverlay(this);
+};
+
+CanvasLayer.prototype.setZIndex = function (zIndex) {
+    this.canvas.style.zIndex = zIndex;
+};
+
+CanvasLayer.prototype.getZIndex = function () {
+    return this.zIndex;
+};
+
+var PointLine = function PointLine(map, userOptions) {
+    var self = this;
+
+    //默认参数
+    var options = {
+        //线条宽度
+        lineWidth: 1,
+        //线条颜色
+        lineStyle: '#F9815C'
+    };
+
+    //全局变量
+    var baseLayer = null,
+        width = map.getSize().width,
+        height = map.getSize().height,
+        lines = [];
+
+    //参数合并
+    var merge = function merge(userOptions, options) {
+        Object.keys(userOptions).forEach(function (key) {
+            options[key] = userOptions[key];
+        });
+    };
+
+    function Line(opts) {
+        this.name = opts.name;
+        this.path = opts.path;
+    }
+
+    Line.prototype.getPointList = function () {
+        var points = [],
+            path = this.path;
+        if (path && path.length > 0) {
+            path.forEach(function (p) {
+                points.push({
+                    name: p.name,
+                    pixel: map.pointToPixel(p.location)
+                });
+            });
+        }
+        return points;
+    };
+
+    Line.prototype.draw = function (context) {
+        var pointList = this.getPointList();
+        context.save();
+        context.beginPath();
+        context.lineWidth = options.lineWidth;
+        context.strokeStyle = options.lineStyle;
+        context.moveTo(pointList[0].pixel.x, pointList[0].pixel.y);
+        for (var i = 0, len = pointList.length; i < len; i++) {
+            context.lineTo(pointList[i].pixel.x, pointList[i].pixel.y);
+        }
+        context.stroke();
+        context.closePath();
+        context.restore();
+    };
+
+    //底层canvas渲染，标注，线条
+    var brush = function brush() {
+        var baseCtx = baseLayer.canvas.getContext('2d');
+        if (!baseCtx) {
+            return;
+        }
+
+        addLine();
+
+        baseCtx.clearRect(0, 0, width, height);
+
+        lines.forEach(function (line) {
+            line.draw(baseCtx);
+        });
+    };
+
+    var addLine = function addLine() {
+        lines = [];
+        var dataset = options.data;
+        dataset.forEach(function (l, i) {
+            var line = new Line({
+                name: l.name,
+                path: []
+            });
+            l.data.forEach(function (p, j) {
+                line.path.push({
+                    name: p.name,
+                    location: new BMap.Point(p.Longitude, p.Latitude)
+                });
+            });
+            lines.push(line);
+        });
+    };
+
+    var init = function init(map, options) {
+        merge(userOptions, options);
+
+        baseLayer = new CanvasLayer({
+            map: map,
+            update: brush
+        });
+    };
+
+    init(map, options);
+
+    self.options = options;
+};
+
+return PointLine;
+
+})));
