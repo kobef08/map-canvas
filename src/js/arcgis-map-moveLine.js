ef0a30d04bd0ecaf1a90d09b06aae4e8f80cada7
chengquan223 <309581454@qq.com>
2018-01-24 00:29:43 +0800

    update build

--- src/js/arcgis-map-moveLine.js
+++ src/js/arcgis-map-moveLine.js
@@ -14,7 +14,7 @@ var MoveLine = function (map, userOptions) {
     //默认参数
     var options = {
         //线条宽度
-        lineWidth: 2,
+        lineWidth: 1,
         //线条颜色
         lineStyle: '#ba92f1'
     };
@@ -23,6 +23,7 @@ var MoveLine = function (map, userOptions) {
 
     //全局变量
     var baseCtx = self.baseCtx = self.options.canvas.getContext("2d");
+    var animateCtx = self.animateCtx = self.options.animateCanvas.getContext("2d");
     baseCtx.lineWidth = options.lineWidth;
 }
 
@@ -41,29 +42,47 @@ MoveLine.prototype.render = function () {
 
     self._addLine();
 
+    self.pixelList = [];
+    self.lines.forEach(function (line) {
+        self.pixelList.push({
+            data: line.getPointList(self.map)
+        })
+        line.drawPath(baseCtx, self.map, self.options);
+    });
+}
+MoveLine.prototype.animate = function () {
+    var self = this;
+    var animateCtx = self.animateCtx;
+    if (!animateCtx) {
+        return;
+    }
+
+    self._addLine();
+
     // baseCtx.clearRect(0, 0, self.map.width, self.map.height);
-    baseCtx.fillStyle = "rgba(0, 0, 0, 0.88)";
-    var prev = baseCtx.globalCompositeOperation;
-    baseCtx.globalCompositeOperation = "destination-in";
-    baseCtx.fillRect(0, 0, self.map.width, self.map.height);
-    baseCtx.globalCompositeOperation = prev;
+    animateCtx.fillStyle = "rgba(0, 0, 0, 0.6)";
+    var prev = animateCtx.globalCompositeOperation;
+    animateCtx.globalCompositeOperation = "destination-in";
+    animateCtx.fillRect(0, 0, self.map.width, self.map.height);
+    animateCtx.globalCompositeOperation = prev;
 
     self.pixelList = [];
     self.lines.forEach(function (line) {
         self.pixelList.push({
             data: line.getPointList(self.map)
         })
-        line.draw(baseCtx, self.map, self.options);
+        line.draw(animateCtx, self.map, self.options);
     });
 }
 
 MoveLine.prototype.start = function () {
     var self = this;
     self.stop();
+    self.render();
     (function drawFrame() {
         self.timer = setTimeout(function () {
             requestAnimationFrame(drawFrame);
-            self.render();
+            self.animate();
         }, 1000 / 20);
     })();
 }
@@ -114,13 +133,28 @@ Line.prototype.getPointList = function (map) {
     return points;
 }
 
+Line.prototype.drawPath = function (context, map, options) {
+    var pointList = this.pixelList || this.getPointList(map);
+    context.save();
+    context.beginPath();
+    context.lineWidth = options.lineWidth;
+    context.strokeStyle = options.lineStyle;
+    context.moveTo(pointList[0].pixel.x, pointList[0].pixel.y);
+    for (var i = 0, len = pointList.length; i < len; i++) {
+        context.lineTo(pointList[i].pixel.x, pointList[i].pixel.y);
+    }
+    context.stroke();
+    context.closePath();
+    context.restore();
+}
+
 Line.prototype.draw = function (context, map, options) {
     var pointList = this.pixelList || this.getPointList(map);
     context.beginPath();
     // context.shadowColor = '#eda513';
     // context.shadowBlur = 2;
     context.lineWidth = options.lineWidth;
-    context.strokeStyle = options.lineStyle;
+    context.strokeStyle = 'yellow';
     if (this.age >= this.maxAge - 1) {
         this.age = 0;
     }
