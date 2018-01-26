97ec128ab1e58c853ba42442a02de1657a586bd0
chengquan223 <309581454@qq.com>
2018-01-26 17:52:37 +0800

    fix resolutionScale bug

--- src/canvas/resolutionScale.js
+++ src/canvas/resolutionScale.js
@@ -1,7 +1,7 @@
 export default function (context) {
     var devicePixelRatio = window.devicePixelRatio || 1;
     context.canvas.width = context.canvas.width * devicePixelRatio;
-    context.cavas.height = context.canvas.height * devicePixelRatio;
+    context.canvas.height = context.canvas.height * devicePixelRatio;
     context.canvas.style.width = context.canvas.width / devicePixelRatio + 'px';
     context.canvas.style.height = context.canvas.height / devicePixelRatio + 'px';
     context.scale(devicePixelRatio, devicePixelRatio);
