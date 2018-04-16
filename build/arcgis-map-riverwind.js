fbd014963b560d8562cf66cbcf0910f5188f403f
chengquan223 <309581454@qq.com>
2018-04-16 18:51:44 +0800

    update arcgis-map-riverwind

--- build/arcgis-map-riverwind.js
+++ build/arcgis-map-riverwind.js
@@ -50,6 +50,10 @@ var resolutionScale = function (context) {
 
 var global = typeof window === 'undefined' ? {} : window;
 
+var requestAnimationFrame = global.requestAnimationFrame || global.mozRequestAnimationFrame || global.webkitRequestAnimationFrame || global.msRequestAnimationFrame || function (callback) {
+    return global.setTimeout(callback, 1000 / 60);
+};
+
 var cancelAnimationFrame = global.cancelAnimationFrame || global.mozCancelAnimationFrame || global.webkitCancelAnimationFrame || global.msCancelAnimationFrame || function (id) {
     clearTimeout(id);
 };
@@ -72,6 +76,8 @@ var Windy = function Windy(map, userOptions) {
 
     //全局变量
     var baseCtx = self.baseCtx = self.options.canvas.getContext("2d");
+    self.baseCtx.lineWidth = 0.8;
+    self.baseCtx.fillStyle = 'rgba(0,0,0,.9)';
 };
 
 Windy.prototype.init = function (setting, defaults) {
@@ -85,15 +91,19 @@ Windy.prototype.start = function () {
     self.stop();
     self.adjustSize();
     self.addLine();
-    if (self.options.isShowLine) {
-        self.drawline();
-    }
-    self.createParticle();
-    self.drawParticle();
+    self.interpolateGrid();
+    // if (self.options.isShowLine) {
+    //     self.drawline();
+    // }
+    // self.createParticle();
+    // self.animate();
+    // self.drawParticle();
 };
 
 Windy.prototype.stop = function () {
     var self = this;
+    self.baseCtx.clearRect(0, 0, self.width, self.height);
+    if (self.vector) self.vector.release();
     if (self.animationId) {
         cancelAnimationFrame(self.animationId);
     }
@@ -103,11 +113,13 @@ Windy.prototype.stop = function () {
 };
 
 Windy.prototype.adjustSize = function () {
-    var width = this.width = this.map.width;
-    var height = this.height = this.map.height;
+    var width = this.map.width;
+    var height = this.map.height;
     this.baseCtx.canvas.width = width;
     this.baseCtx.canvas.height = height;
     resolutionScale(this.baseCtx);
+    this.width = this.baseCtx.canvas.width;
+    this.height = this.baseCtx.canvas.height;
 };
 
 Windy.prototype.addLine = function () {
@@ -149,14 +161,84 @@ Windy.prototype.createParticle = function () {
         }, poly) === 'out') {
             continue;
         }
-        particles.push(new Particle({
-            x: x,
-            y: y,
-            age: Math.floor(Math.random() * options.MAX_PARTICLE_AGE),
-            color: '#fff'
-        }));
+        if (this.columns[x] == undefined) {
+            continue;
+        }
+        var vector = self.vector(x, y);
+        vector.x = x;
+        vector.y = y;
+        vector.age = Math.floor(Math.random() * options.MAX_PARTICLE_AGE);
+        vector.color = '#fff';
+        particles.push(vector);
     }
-    console.log(particles.length);
+};
+
+Windy.prototype.animate = function () {
+    var self = this,
+        options = this.options,
+        particles = self.particles;
+
+    function evolve() {
+        particles.forEach(function (particle, i) {
+            if (particle.age > options.MAX_PARTICLE_AGE) {
+                particle = self.vector.generate({
+                    age: 0
+                });
+                particles.splice(i, 1, particle);
+            }
+            var x = particle.x;
+            var y = particle.y;
+            var v = self.vector(x, y);
+            if (v) {
+                var xe = x - v.speed * Math.sin(Math.PI / 180 * (180 - v.angle));
+                var ye = y - v.speed * Math.cos(Math.PI / 180 * (180 - v.angle));
+                var nextPoint = self.vector(xe, ye);
+                if (nextPoint) {
+                    particle.xe = xe;
+                    particle.ye = ye;
+                } else {
+                    var newParticle = self.vector.generate({
+                        age: Math.floor(Math.random() * options.MAX_PARTICLE_AGE)
+                    });
+                    particles.splice(i, 1, newParticle);
+                }
+            } else {
+                particle.age = options.MAX_PARTICLE_AGE;
+            }
+            particle.age += 1;
+        });
+    }
+
+    function render() {
+        var context = self.baseCtx;
+        context.fillStyle = "rgba(0,0,0,0.97)";
+        var prev = context.globalCompositeOperation;
+        context.globalCompositeOperation = "destination-in";
+        context.fillRect(0, 0, self.width, self.height);
+        context.globalCompositeOperation = prev;
+
+        context.beginPath();
+        context.strokeStyle = '#fff';
+        particles.forEach(function (particle, i) {
+            context.moveTo(particle.x, particle.y);
+            context.lineTo(particle.xe, particle.ye);
+            particle.x = particle.xe;
+            particle.y = particle.ye;
+        });
+        context.stroke();
+    }
+
+    (function frame() {
+        try {
+            self.timer = setTimeout(function () {
+                requestAnimationFrame(frame);
+                evolve();
+                render();
+            }, 1000 / options.FRAME_RATE);
+        } catch (e) {
+            console.error(e);
+        }
+    })();
 };
 
 Windy.prototype.drawParticle = function () {
@@ -171,7 +253,88 @@ Windy.prototype.drawParticle = function () {
     });
 };
 
-Windy.prototype.interpolate = function (x, y) {};
+Windy.prototype.interpolateGrid = function () {
+    var self = this;
+    var columns = this.columns = [];
+    var x = 0;
+
+    function interpolateColumn(x) {
+        var column = [];
+        for (var y = 0; y < self.height; y += 2) {
+            var wind = interpolate(x, y);
+            column[y + 1] = column[y] = wind;
+        }
+        columns[x + 1] = columns[x] = column;
+    }
+
+    var points = self.roadLines[0].points.splice(0, 90);
+
+    function interpolate(x, y) {
+        var angle0 = 0,
+            angle1 = 0,
+            speed0 = 5,
+            speed1 = 5,
+            wind = {};
+        points.forEach(function (p) {
+            angle0 += p.angle * 1.0 / ((y - p.y) * (y - p.y) + (x - p.x) * (x - p.x));
+            angle1 += 1.0 / ((y - p.y) * (y - p.y) + (x - p.x) * (x - p.x));
+
+            speed0 += p.speed * 1.0 / ((y - p.y) * (y - p.y) + (x - p.x) * (x - p.x));
+            speed1 += 1.0 / ((y - p.y) * (y - p.y) + (x - p.x) * (x - p.x));
+
+            if (angle1 != 0) {
+                wind.angle = angle0 / angle1;
+            }
+            if (speed1 != 0) {
+                wind.speed = speed0 / speed1;
+            }
+        });
+        return wind;
+    }
+
+    (function batchInterpolate() {
+        var start = Date.now();
+        while (x < self.width) {
+            interpolateColumn(x);
+            x += 2;
+            if (Date.now() - start > 1000) {
+                setTimeout(batchInterpolate, 25);
+                return;
+            }
+        }
+        self.createField();
+    })();
+};
+
+Windy.prototype.createField = function () {
+    var self = this;
+    var columns = this.columns;
+
+    function vector(x, y) {
+        var column = columns[Math.floor(x)];
+        return column && column[Math.floor(y)];
+    }
+
+    vector.release = function () {
+        self.columns = [];
+    };
+
+    vector.generate = function (o) {
+        var x = Math.floor(Math.random() * self.width);
+        var y = Math.floor(Math.random() * self.height);
+        o.x = x;
+        o.y = y;
+        return o;
+    };
+
+    this.vector = vector;
+
+    if (this.options.isShowLine) {
+        this.drawline();
+    }
+    this.createParticle();
+    this.animate();
+};
 
 Windy.prototype.rayCasting = function (p, poly) {
     var px = p.x,
@@ -210,20 +373,6 @@ Windy.prototype.rayCasting = function (p, poly) {
     return flag ? 'in' : 'out';
 };
 
-function Particle(options) {
-    this.x = options.x;
-    this.y = options.y;
-    this.age = options.age;
-    this.color = options.color;
-}
-
-Particle.prototype.draw = function (context) {
-    context.beginPath();
-    context.arc(this.x, this.y, 1, 0, Math.PI * 2);
-    context.fillStyle = this.color;
-    context.fill();
-};
-
 function Line(options) {
     this.points = options.points || [];
     this.color = options.color || '#ffff00';
@@ -254,7 +403,6 @@ Line.prototype.drawPath = function (context, map) {
     context.beginPath();
     context.lineWidth = 1;
     context.strokeStyle = this.color;
-    // context.strokeStyle = options.lineStyle;
     context.moveTo(pointList[0].x, pointList[0].y);
     for (var i = 0, len = pointList.length; i < len; i++) {
         context.lineTo(pointList[i].x, pointList[i].y);
